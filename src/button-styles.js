4d05f76c42b5d236b2bd498893fd8f363f3fb77b
Lisa Ross <lr@footprintleadership.com>
2018-09-19 12:27:13 -0400

    :art: refactor modules
    refactor modules

---
+++ src/button-styles.js
@@ -0,0 +1,7 @@
+const redButton = "color: red;";
+const blueButton = "color:blue;";
+const makeColorStyle = color => `color: ${color};`;
+
+exports.red = red;
+exports.blue = blue;
+exports.makeColorStyle = makeColorStyle;
---
+++ src/button.js
@@ -0,0 +1,13 @@
+// take a str, the button label and return an element
+
+/**
+ *
+ *
+ * @param {string} buttonName
+ * @returns {Element}
+ */
+const makeButton = buttonName => {
+  return `Button: ${buttonName}`;
+};
+
+module.exports = makeButton;
