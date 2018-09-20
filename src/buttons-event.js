e047f3995ab1da19949580045aa05a2b8fef5d23
Lisa Ross <lr@footprintleadership.com>
2018-09-20 15:37:29 -0400

    :sparkles: add button click tracker
    add script - mixpanel which button clicked

---
+++ src/buttons-event.js
@@ -0,0 +1,17 @@
+function buttonEvent() {
+  var allButtons = document.getElementsByTagName("button");
+  console.log("Button List: ");
+  for (i = 0; i < allButtons.length; i++) {
+    (function(index) {
+      var buttonName = allButtons[index].name;
+      allButtons[index].onclick = function() {
+        mixpanel.track(`${buttonName} clicked`);
+        console.log("I am button " + buttonName);
+      };
+    })(i);
+  }
+}
+
+window.addEventListener("load", function() {
+  buttonEvent();
+});
